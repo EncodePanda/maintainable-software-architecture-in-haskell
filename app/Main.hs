767b7c7973ff798ecbebce7e475da49626750c57
Pawel Szulc <paul.szulc@gmail.com>
2020-02-13 09:47:09 +0100

    Billing: use interpreters in main

--- app/Main.hs
+++ app/Main.hs
@@ -1,19 +1,51 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import           BS.Effects.CdrStore
+import           BS.Effects.Crm
+import           BS.Effects.InvoiceStore
+import           BS.GenerateInvoice
+import           BS.Types
+import           Data.Aeson
+import           Data.Aeson.Encode.Pretty
+import           Data.ByteString.Char8    (unpack)
+import           Data.ByteString.Lazy     (toStrict)
+import           Data.Function            ((&))
 import           Data.IORef
 import           Data.IORef
-import qualified Data.Map     as M
-import           Data.UUID.V4 (nextRandom)
+import qualified Data.Map                 as M
+import           Data.UUID
+import           Data.UUID.V4
 import           FM.Fm
 import           FM.Free
+import           Polysemy
+import           Polysemy.State
 
 main :: IO ()
-main = do
-  uuid <- nextRandom
-  ioRef <- newIORef $ M.singleton uuid 5
-  res <- interpretFree (interpret ioRef) (program uuid)
-  putStrLn res
+main = execute >>= putStrLn.unpack.toStrict.encodePretty
+  where
+    accountId = AccountId 1000
+    execute = generateInvoice accountId
+      & runCrm
+      & runCdrStore
+      & runInvoiceStore
+      & evalState @CrmMap (M.singleton accountId profile)
+      & evalState @CdrMap (M.singleton accountId (cdrs accountId))
+      & evalState @InvoiceMap M.empty
+      & runM
 
-program uuid = do
-  doStuff uuid 10
-  doStuff uuid 20
+profile :: Profile
+profile = Profile "John" "Smith" address plan
+  where
+    address =
+      Address "Backer Street" "221b" "2" "London" "United Kingdom"
+    plan = Plan 10 1
+
+cdrs :: AccountId -> [Cdr]
+cdrs accountId = [ cdr "8abbe08f-4b64-4263-b000-13f3ff77a0c6" Voice 10
+       , cdr "bed067b0-3e79-429d-8b96-d1f2c96e79ba" Sms 1
+       , cdr "d4bea3d9-a2a7-44cc-8a8d-301051860761" Voice 30
+       ]
+  where
+    cdr uuid callType duration =
+      Cdr (maybe nil id (fromString uuid)) accountId callType (Duration duration)
--- src/BS/Types.hs
+++ src/BS/Types.hs
@@ -1,18 +1,21 @@
 module BS.Types where
 
+import           Data.Aeson
 import           Data.Text
+import           GHC.Generics
 
 newtype AccountId  = AccountId { unAccountId :: Int }
-  deriving (Show, Eq, Ord)
+  deriving stock (Show, Eq, Ord)
+  deriving newtype (ToJSON, FromJSON)
 
 newtype InvoiceNumber = InvoiceNumber { unInvoiceNumber :: Text }
   deriving (Show, Eq, Ord)
+  deriving newtype (ToJSON, FromJSON)
 
 type Cent = Int
 
 zeroCents :: Cent
 zeroCents = 0
-
 
 data Address = Address
   { street  :: Text
@@ -21,11 +24,19 @@ data Address = Address
   , city    :: Text
   , country :: Text
   }
+  deriving stock (Show, Generic)
+
+instance FromJSON Address
+instance ToJSON Address
 
 data FullName = FullName
   { first :: Text
   , last  :: Text
   }
+  deriving stock (Show, Generic)
+
+instance FromJSON FullName
+instance ToJSON FullName
 
 data Invoice = Invoice
   { invoiceNumber   :: InvoiceNumber
@@ -33,3 +44,7 @@ data Invoice = Invoice
   , deliveryAddress :: Address
   , total           :: Cent
   }
+  deriving stock (Show, Generic)
+
+instance FromJSON Invoice
+instance ToJSON Invoice
